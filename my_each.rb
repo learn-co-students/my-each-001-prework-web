cb9c1000db1c80a76f68fd2b0087cc44159a76e5
matthewbillienyc <matthewbillienyc@gmail.com>
2015-08-31 17:51:38 -0400

    updated

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,19 @@
+#def my_each(words)
+#   # put argument(s) here
+#  # code here
+#  while words.size > 0
+#    x = words.shift
+#    yield(x)
+#  end
+#  words
+#end
+
+
 def my_each(words)
-   # put argument(s) here
-  # code here
-  while words.size > 0
-    x = words.shift
-    yield(x)
+  x = 0
+  while words[x] != nil
+    yield(words[x])
+    x += 1
   end
   words
 end
