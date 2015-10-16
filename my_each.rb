863128b0861c3974241f543d7847cfa518dbf4fa
Dylan Brevis <dylanito2@gmail.com>
2015-10-16 00:15:07 -0400

    done

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  count = 0
+  while count < array.size
+    value = array[count]
+    yield(value)
+    count += 1
+  end
+  array
 end
