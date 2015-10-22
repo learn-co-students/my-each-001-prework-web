8827e016f9538633163c6740e3034bb37d8a592f
Louiefigz <figueroa.aaron@gmail.com>
2015-10-21 20:31:38 -0400

    my_each

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,20 @@
 def my_each(array)
-  re_use=Proc.new {|word| puts word}
-  new_array = array.select(&re_use)
-  array 
+ counter = 0  
+  while counter < array.length
+    yield counter
+    counter +=1
 end
+array
+
+end 
+
+
+
+
+
+# def my_each(array)
+#   re_use=Proc.new {|word| puts word}
+#   new_array = array.select(&re_use)
+#   array 
+# end
+
