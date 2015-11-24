6eca0048b0910b9b8fb516c3eb4f6abe53331fb1
Munish Chaturvedi <munishchaturvedi1@gmail.com>
2015-11-24 17:49:05 -0600

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  i = 0
+    until i >= (collection.length) do
+      yield(collection[i])
+      i+=1
+    end
+  return collection
 end
+
+
+my_each(['hi', 'hello', 'bye', 'goodbye']) { 
+  |item| 
+  puts item 
+}
