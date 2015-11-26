2784a2e1e42b55f85d7f53d08a4d3608115d6469
burkefielder <burke.fielder@gmail.com>
2015-11-25 18:22:40 -0800

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(arg1)
+  if block_given?
+   counter = 0
+    until counter == arg1.length do 
+      yield(arg1[counter])
+      counter += 1
+    end
+    arg1
+  end
+
 end
