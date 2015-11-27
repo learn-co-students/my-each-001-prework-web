12875feb95a81b0deb37efd7f7fbdcc7364fcdd5
juliascript <juliageist23@gmail.com>
2015-11-27 18:14:00 +0000

    nitrous-backup at Fri Nov 27 18:14:00 UTC 2015

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
+def my_each(array)# put argument(s) here
   # code here
+  i = 0
+  while i < array.size do
+    each = array[i]
+    yield(each)
+    i += 1
+  end
 end
+
+my_each { |each| puts each }
