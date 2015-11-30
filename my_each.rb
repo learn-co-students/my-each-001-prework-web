6c547ebc8eaae8c85be985208263d62f0879f059
wuandy91 <wuandy91@gmail.com>
2015-11-29 22:04:56 -0500

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
+def my_each(words)# put argument(s) here
   # code here
+  
+  new_array = []
+  
+
+  for i in (0...words.length)
+  	yield(words[i])
+  	new_array << words[i]
+  	i+=1
+  	
+  end
+  new_array
+
+
 end
