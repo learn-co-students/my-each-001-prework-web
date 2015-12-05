341770c264aff7bac5b2e8168bcc8e76ff39bfc2
WilliamAG <william.a.gough@gmail.com>
2015-12-04 21:11:25 -0500

    Done

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,19 @@
-def my_each # put argument(s) here
+def my_each (input)# put argument(s) here
   # code here
+  # if input.kind_of?(Array)
+  #  n=0
+  #   until n==input.length
+  #     yield input[n]
+  #     n+=1
+  #   end
+  # else
+  #   # input.join
+  # end
+  # input
+  
+  x= input.length
+  x.times do |i|
+    yield input[i]
+  end
+  input  
 end
