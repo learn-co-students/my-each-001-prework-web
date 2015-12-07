9f43b22b0b5ce2323d417385e02cdeaff24cea61
devinburnette <devin.burnette@gmail.com>
2015-12-07 21:11:38 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  array.collect do |x|
+    yield(x)
+  end
+  array
 end
+
+my_each([1,2,3,4]) { |x| x }
+
