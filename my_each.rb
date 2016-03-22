fbcb21b59fb74b2849b3b1fd17dcbf707d8bdfd0
jmkiley <jordankiley@gmail.com>
2016-03-22 19:08:16 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+  i = array.length
+  while i > 0
+    array.map do |item|
+      yield item
+      i -= 1
+    end
+  end
+  array  # code here
 end
