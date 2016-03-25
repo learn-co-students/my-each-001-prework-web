f4fce69a826914339314d9c732b677a0efd89f8f
chriswebb09 <chris.webb5249@gmail.com>
2016-03-25 12:42:14 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+def my_each(greeting_array)# put argument(s) here
   # code here
+  i = 0
+  while i < greeting_array.length
+    yield(greeting_array[i])
+    i += 1
+  end
+  return greeting_array
 end
