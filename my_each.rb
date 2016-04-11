12fa2c6494da64785642f4d4a2108fb666b4a5df
BettinaP <bettinaprophete@gmail.com>
2016-04-10 22:32:55 -0400

    my_each 2

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,9 @@
 def my_each (collection)
-  if block_given?
     i = 0
     while i < collection.length
       yield(collection[i])
       i += 1
     end
     collection
-  else
-    "This block should not run!"
-  end
+
 end
