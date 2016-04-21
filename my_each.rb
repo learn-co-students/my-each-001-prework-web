d02b2cdbf0a7ff8ef0a8e87f43392cbce3c3c893
samanthabertous <sam@samanthabertous.com>
2016-04-21 11:26:10 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
+#words = ['hi', 'hello', 'bye', 'goodbye']
+
+def my_each(collection) # put argument(s) here
   # code here
+  i=0
+  while i < collection.length
+  	yield collection[i] 
+  	i+=1
+  end
+  collection
 end
+
+
+
