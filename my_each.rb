2981fc41a097d1933c9e857c7df0e6294c756e85
gnwankwo <gnwankwo03@gmail.com>
2016-05-26 15:47:42 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (classmates)
+  counter =0
+  current_classsmates=classmates.clone
+  while counter < classmates.length 
+    yield classmates[counter] 
+    counter+=1
+  end
+  return current_classsmates
 end
+
+
+
