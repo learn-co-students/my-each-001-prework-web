5be0a01dc3a51c13ecb01a97fde26f682b10cb16
iliass sabillah <iliass sabillah>
2016-06-02 03:12:28 -0400

    my each method

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,8 @@
 def my_each(array)
-  count=0
-  while count < array.length
-    yield(array[count])
-    count+=1
-  end
-  array
+    count=0
+    while count < array.length
+      yield(array[count])
+      count+=1
+    end
+    array
 end
-my_each([1,2,3,4]) {|x|  x }
