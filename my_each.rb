b14df97eb1bdaab2d56d438ff8f22e9da62e3fb1
Tami <catquarks@gmail.com>
2016-06-18 00:42:35 -0400

    Passed tests.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,19 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i = 0
+  while i < array.size
+  	yield array[i]
+  	i +=1
+	end
+	array
 end
+
+collection = [(1..10).to_a]
+cats = %w[fluffy Persian tabby cute mewy whiskery]
+
+my_each(collection) { |i| i } # can't do math without getting TypeErrror :(
+ 
+my_each(cats) {|cat| "I like #{cat} cats."}
+
+
+# it's not called with collection.my_each because
+# it calls a block
