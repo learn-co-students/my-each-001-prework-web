efd54818e682f2e52f5233e012a23277b2edbb20
lshapz <likeaduck@gmail.com>
2016-06-21 14:44:02 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
+def my_each(thing) # put argument(s) here
+  #thing.collect do |thi|
+  i = 0
+  while i < thing.length
+   yield thing[i] 
+
+   i += 1
+  end 
+ # end
+thing
   # code here
 end
