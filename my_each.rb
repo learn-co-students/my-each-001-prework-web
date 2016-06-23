31811e254954eb86469f5e1965e11320fce2a1eb
ivanjanko <jankoemail@gmail.com>
2016-06-23 11:40:55 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+coll = [1, 2, 3, 4]
+
+def my_each(coll)
+  i = 0
+  while i < coll.length
+    yield(coll[i])
+    coll[i]
+    i = i + 1
+  end
+  coll
 end
+
