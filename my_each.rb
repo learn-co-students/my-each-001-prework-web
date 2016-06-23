82764e19e6058b25bb90fc51c26d99e1b0c064a3
Dillon Powers <dillon.c.powers@gmail.com>
2016-06-23 10:39:45 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i = 0
+  while i < array.length # starts iteration
+    yield(array[i]) # execute block of code on array element with index i
+    i += 1 # move to next iterable
+  end
+  array # #each returns original array
 end
