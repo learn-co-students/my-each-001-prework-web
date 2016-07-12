336599ed4a9945422e2b3406a64cc2c96bd18875
Dallin Wardrop <dallinwardrop12@gmail.com>
2016-07-12 14:57:16 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,8 @@
-def my_each (word) # put argument(s) here
-  while x > 1 do
-    puts word
-    yield
+def my_each(collection)
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i = i + 1
   end
-  # code here
+  collection
 end
