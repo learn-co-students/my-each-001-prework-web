8233d69d79528a680c1509569cb9ad61bffa102a
Aynalem Lenjiso <aynalem.lenjiso@gmial.com>
2016-08-03 13:32:28 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  newarray =[]
+  count = 0
+  while count < array.length
+    yield   array[count]
+     count += 1
+     end
+     return array
 end
