c96219829605017862a2cfc6320cc57c893d54f0
Michael Wiggins <michael.wiggins@me.com>
2016-08-17 16:34:01 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,20 @@
 def my_each (array)
 i = 0
-while i < array.length
-yield array[i]
+  while i < array.length
+    yield array[i]
   i = i + 1
+  end
+  array
 end
-array
-end
+#STEP 1: DEFINE YOUR METHOD TO ACCEPT AN ARGUMENT
+#STEP 2: ENACT AN ITERATION using a while loop, set a counter variable i = 0, increment by 1 on each pass
+#STEP 3: YIELDING TO THE BLOCK yield array[i]
+  #Here, we use the [], bracket, method to grab the value of each successive index element as we proceed through our while loop, and yield it to a block
+#STEP 4: We could use this by calling with the argument of an array:
+  #my_method([array]) do |greeting|
+  #if greeting.start_with?("H")
+  #p-ts#{greeting}, Bob"
+  #end
+  #end
+#STEP 5: To return the original array, you can call it as the penultimate thing in the def.
+#https://learn.co/tracks/web-development-fundamentals/intro-to-ruby/enumerables/yield-and-blocks
