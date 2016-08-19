10418b6b12b0102e213f7a05d7c6ba28dbd92d7d
Michael Wiggins <michael.wiggins@me.com>
2016-08-19 13:58:11 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,20 +1,21 @@
-def my_each (array)
-i = 0
-  while i < array.length
-    yield array[i]
-  i = i + 1
+def my_each (anyArray)
+indexElement = 0
+  while indexElement < anyArray.length
+    yield anyArray[indexElement]
+  indexElement += 1
   end
-  array
+  anyArray
 end
-#STEP 1: DEFINE YOUR METHOD TO ACCEPT AN ARGUMENT
-#STEP 2: ENACT AN ITERATION using a while loop, set a counter variable i = 0, increment by 1 on each pass
-#STEP 3: YIELDING TO THE BLOCK yield array[i]
+#In the code above, we set a counter variable, i, equal to 0.
+#We start our while loop Psuedocode: "while i < the length of the array, keep counting."
+#Inside our while loop, we count, we increment, we iterate
+#We tell our method to yield to the array and the counter chosen by the future programmer...
+#As we loop over it, to the block we will call our #hello_t method with.
+
+#STEP 1: DEFINE YOUR METHOD TO ACCEPT AN ARGUMENT (array)
+#STEP 2: ENACT AN ITERATION using a while loop. Set a counter i = 0, increment by 1 on each pass
+#STEP 3: YIELDING TO THE BLOCK yield array[i] Here you gain flexibility. It  can handle any array, and any counter.
   #Here, we use the [], bracket, method to grab the value of each successive index element as we proceed through our while loop, and yield it to a block
-#STEP 4: We could use this by calling with the argument of an array:
-  #my_method([array]) do |greeting|
-  #if greeting.start_with?("H")
-  #p-ts#{greeting}, Bob"
-  #end
-  #end
-#STEP 5: To return the original array, you can call it as the penultimate thing in the def.
+#STEP 4: Call our method with an argument. It takes an array, so any array will do and it will be iterated over as described above.
+#STEP 5: Test
 #https://learn.co/tracks/web-development-fundamentals/intro-to-ruby/enumerables/yield-and-blocks
