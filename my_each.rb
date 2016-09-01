b01374c31f56c57f38a1915a4c4244cb7d85d3c3
wcpines <colby@aptible.com>
2016-09-01 17:12:11 -0400

    scratching an each

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+# all you have to do is make sure yield passes the correct object/item to the block!
+collection = [1, 2, 3, 4]
+def my_each(list)
+  i = 0
+  while i < list.length
+    yield list[i]
+    i += 1
+  end
+  list
 end
